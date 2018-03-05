50b92a5b82a8ca618cbd57c010f18ceb1e3ea7c7
Adam Kollberg <adamkollberg@c213-89-20-124.bredband.comhem.se>
2018-03-05 11:51:36 +0100

    fixed the marker

--- 05_02/script.js
+++ 05_02/script.js
@@ -1,11 +1,19 @@
 var gMapAPIK = "AIzaSyAUcH_W8Tgs-lULk05ISp9IhtmKPg8SrtI";
 var map;
       function initMap() {
+        var storeLocation = {lat:34.1031252, lng: -118.3263873};
+        
         map = new google.maps.Map(document.getElementById('hplus-map'), {
-          'center': {lat:34.1031252, lng: -118.3263873},
-          'zoom': 12,
-          'mapTypeID': google.maps.MapTypeId.ROADMAP,
-          'draggable': false,
-          'scrollwheel': false
+          'center' : storeLocation,
+          'zoom' : 12,
+          'mapTypeId' : google.maps.MapTypeId.ROADMAP,
+          'draggable' : false,
+          'scrollwheel' : false
+        });
+        
+        var marker = new google.maps.Marker({
+          'position': storeLocation,
+          'map': map,
+          'title': 'hplus sport in Los Angeles (actually Capitol Records in real life)'
         });
       }
