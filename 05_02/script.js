8dde73fc1ca342ca3cfd653c4eea2f5b405a058a
Adam Kollberg <adamkollberg@c213-89-20-124.bredband.comhem.se>
2018-03-05 11:23:08 +0100

    fixed the adress

--- 05_02/script.js
+++ 05_02/script.js
@@ -2,7 +2,7 @@ var gMapAPIK = "AIzaSyAUcH_W8Tgs-lULk05ISp9IhtmKPg8SrtI";
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('hplus-map'), {
-          center: {lat: -34.397, lng: 150.644},
+          center: {lat:34.1031252, lng: -118.3263873},
           zoom: 8
         });
       }
