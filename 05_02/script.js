545917db37fa2549aafc37f4ebb12d14d35235a6
Adam Kollberg <adamkollberg@c213-89-20-124.bredband.comhem.se>
2018-03-05 11:38:25 +0100

    fixed the scroll and zoom

--- 05_02/script.js
+++ 05_02/script.js
@@ -2,7 +2,10 @@ var gMapAPIK = "AIzaSyAUcH_W8Tgs-lULk05ISp9IhtmKPg8SrtI";
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('hplus-map'), {
-          center: {lat:34.1031252, lng: -118.3263873},
-          zoom: 8
+          'center': {lat:34.1031252, lng: -118.3263873},
+          'zoom': 12,
+          'mapTypeID': google.maps.MapTypeId.ROADMAP,
+          'draggable': false,
+          'scrollwheel': false
         });
       }
